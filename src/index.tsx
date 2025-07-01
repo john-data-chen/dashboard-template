b8fc3f1e64fb29e3ba959e40ee1e84e1367fb209
JianAn Chen <john.data.chen@gmail.com>
2025-07-01 15:09:47 +0800

    chore: init project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
