c6094355c922208af76f99544e770c4b625242d4
JianAn Chen <john.data.chen@gmail.com>
2025-07-20 16:13:00 +0800

    feat: add query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryProvider } from './providers/QueryProvider';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <QueryProvider>
+        <App />
+      </QueryProvider>
     </React.StrictMode>
   );
 }
