9e639d3a2acd23e2fe0bc21e8a2565237437ff80
JianAn Chen <john.data.chen@gmail.com>
2025-07-20 11:01:59 +0800

    chore: remove temp layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import './App.css';
-import Button from './components/Button';
-import Title from './components/Title';
-import { useState } from 'react';
+import './global.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const onClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div className="content">
-      <Title />
-      <p>{count}</p>
-      <Button onClick={onClick} />
-    </div>
-  );
+  return <div className="content"></div>;
 };
 
 export default App;
